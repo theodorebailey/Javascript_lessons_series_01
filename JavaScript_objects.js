f90403ba27c749141f130972b07a4db564a3a7ef
Theodore Bailey <tawbailey@hotmail.com>
2023-07-20 10:01:56 +0100

    initial object commit

---
+++ JavaScript_objects.js
@@ -0,0 +1,26 @@
+let radius = 1;
+let x = 1;
+let y = 1;
+
+// group related variables
+// add functions 
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    isVisible: true,
+    function draw() {
+        console.log("draw")
+    },
+    function move() {
+        console.log("move")
+    }
+}
+
+// add functions to an object
+// function draw() {}
+// function move() {}
+
+circl
